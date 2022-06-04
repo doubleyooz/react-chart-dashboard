2c6f0a2939975401518210fcc7d023e88b696538
Waifu Forever <itachiut1r4@gmail.com>
2022-06-04 14:24:27 -0300

    add: tailwind themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,26 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            textColor: {
+                skin: {
+                    solid: 'var(--color-text-solid)',
+                    medium: 'var(--color-text-medium)',
+                    mild: 'var(--color-text-medium)',
+                },
+            },
+            backgroundColor: {
+                skin: {
+                    default: 'var(--color-bg-default)',
+                    detail_1: 'var(--color-bg-detail-1)',
+                },
+            },
+            fill: {
+                skin: {
+                    default: 'var(--color-icon-image-1)',
+                },
+            },
+        },
     },
     plugins: [],
 };
