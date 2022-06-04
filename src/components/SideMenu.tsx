04cbf391ff78ae4bfa0c7df8d9495d6729c367de
Waifu Forever <itachiut1r4@gmail.com>
2022-06-04 14:25:31 -0300

    update: dynamic icon color

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -2,25 +2,42 @@ import { useState } from 'react';
 
 import SimpleButton from './SimpleButton';
 
-import logo from '../assets/menu/logo.svg';
-import home from '../assets/menu/home.svg';
-import status from '../assets/menu/status.svg';
-import card from '../assets/menu/card.svg';
-
+import { ReactComponent as Logo } from '../assets/menu/logo.svg';
+import { ReactComponent as Home } from '../assets/menu/home.svg';
+import { ReactComponent as Status } from '../assets/menu/status.svg';
+import { ReactComponent as Card } from '../assets/menu/card.svg';
 
 interface Props {}
 
 const SideMenu: React.FC<Props> = () => {
     const [isSelected, setIsSelected] = useState<string[]>([]);
-    const [isExpanded, setIsExpanded] = useState<string[]>([])
-    
+    const [isExpanded, setIsExpanded] = useState<string[]>([]);
+
     return (
         <div>
-            <img src={logo} alt="" />
-            <SimpleButton icon={home} description="Visão Geral" isActive={isSelected.includes('b')}/>
-            <SimpleButton icon={status} description="Carteira dos clientes" isActive={isExpanded.includes('s')}/>
-            <SimpleButton icon={card} description="Fazenda Agro Mais" isActive={isSelected.includes('b')} deleteButton={true}/>
-            <SimpleButton icon={card} description="Fazenda Agro Top" isActive={isSelected.includes('b')} deleteButton={true}/>
+            <Logo />
+            <SimpleButton
+                Icon={<Home className="fill-skin-default" />}
+                description="Visão Geral"
+                isActive={isSelected.includes('b')}
+            />
+            <SimpleButton
+                Icon={<Status className="fill-skin-default" />}
+                description="Carteira dos clientes"
+                isActive={isExpanded.includes('s')}
+            />
+            <SimpleButton
+                Icon={<Card className="fill-skin-default" />}
+                description="Fazenda Agro Mais"
+                isActive={isSelected.includes('b')}
+                deleteButton={true}
+            />
+            <SimpleButton
+                Icon={<Card className="fill-skin-default" />}
+                description="Fazenda Agro Top"
+                isActive={isSelected.includes('b')}
+                deleteButton={true}
+            />
         </div>
     );
 };
--- src/components/SimpleButton.tsx
+++ src/components/SimpleButton.tsx
@@ -1,25 +1,25 @@
 interface Props {
-    icon: string;
+    Icon: React.ReactNode;
     description: string;
     isActive: boolean;
     deleteButton?: boolean;
 }
 
 const SimpleButton: React.FC<Props> = ({
-    icon,
+    Icon,
     description,
     isActive,
     deleteButton,
 }) => {
     return (
         <div className="flex justify-start items-center gap-[11.5px]">
-            <img
-                className="bg-slate-500 w-[30px] h-[30px] p-[7.5px] rounded-xl "
-                src={icon}
-                alt=""
-            />
+            <div className="bg-skin-detail_1 p-2">{Icon}</div>
 
-            <span className="font-['Helvetica'] text-[#A0AEC0] group-hover:text-white">
+            <span
+                className={`${
+                    isActive ? 'text-skin-solid' : 'text-skin-mild'
+                } font-bold font-['Helvetica']  group-hover:text-white`}
+            >
                 {description}
             </span>
             {deleteButton && (
