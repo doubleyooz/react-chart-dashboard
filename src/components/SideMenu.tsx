805da36e44dd5f66e8c214dfedefd2a49e14c8c5
Waifu Forever <itachiut1r4@gmail.com>
2022-06-04 11:35:25 -0300

    update: SimpleButton properties

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,12 +1,26 @@
-import logo from '../assets/menu/logo.svg';
-import home from '../assets/menu/home.svg';
+import { useState } from 'react';
+
 import SimpleButton from './SimpleButton';
 
-const SideMenu = () => {
+import logo from '../assets/menu/logo.svg';
+import home from '../assets/menu/home.svg';
+import status from '../assets/menu/status.svg';
+import card from '../assets/menu/card.svg';
+
+
+interface Props {}
+
+const SideMenu: React.FC<Props> = () => {
+    const [isSelected, setIsSelected] = useState<string[]>([]);
+    const [isExpanded, setIsExpanded] = useState<string[]>([])
+    
     return (
         <div>
             <img src={logo} alt="" />
-            <SimpleButton icon={home} description='button'/>
+            <SimpleButton icon={home} description="Visão Geral" isActive={isSelected.includes('b')}/>
+            <SimpleButton icon={status} description="Carteira dos clientes" isActive={isExpanded.includes('s')}/>
+            <SimpleButton icon={card} description="Fazenda Agro Mais" isActive={isSelected.includes('b')} deleteButton={true}/>
+            <SimpleButton icon={card} description="Fazenda Agro Top" isActive={isSelected.includes('b')} deleteButton={true}/>
         </div>
     );
 };
--- src/components/SimpleButton.tsx
+++ src/components/SimpleButton.tsx
@@ -1,13 +1,32 @@
 interface Props {
     icon: string;
     description: string;
+    isActive: boolean;
+    deleteButton?: boolean;
 }
 
-const SimpleButton: React.FC<Props> = ({ icon, description }) => {
+const SimpleButton: React.FC<Props> = ({
+    icon,
+    description,
+    isActive,
+    deleteButton,
+}) => {
     return (
-        <div className="bg-slate-500">
-            <img src={icon} alt="" />
-            <span>{description}</span>
+        <div className="flex justify-start items-center gap-[11.5px]">
+            <img
+                className="bg-slate-500 w-[30px] h-[30px] p-[7.5px] rounded-xl "
+                src={icon}
+                alt=""
+            />
+
+            <span className="font-['Helvetica'] text-[#A0AEC0] group-hover:text-white">
+                {description}
+            </span>
+            {deleteButton && (
+                <div className="bg-[#7E869E] text-white rounded-[50%] p-[6px]">
+                    X
+                </div>
+            )}
         </div>
     );
 };
