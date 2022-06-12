4ffe45017e4ae28830abd840826ebc3f2ba96961
Waifu Forever <itachiut1r4@gmail.com>
2022-06-12 11:15:02 -0300

    refactor: using columns and scrolling

--- src/components/SpreadSheet.tsx
+++ src/components/SpreadSheet.tsx
@@ -1,42 +1,71 @@
 interface Props {
     title: string;
-    values: number[];
+    values: string[];
     isLabel?: boolean;
+    isTail?: boolean;
 }
 
-const Line: React.FC<Props> = ({ title, values, isLabel }) => {
+const formatString = (str: string, index: number): string => {
+    let integers = str.slice(0, index);
+    let decimals = str.slice(index);
+
+    return integers.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.') + ',' + decimals;
+};
+
+const Column: React.FC<Props> = ({ title, values, isLabel, isTail }) => {
     return (
-        <div className="flex gap-5 border-b-[1px] my-2">
-            <span className="w-40 text-xs xl:text-sm 2xl:text-base font-bold">{title}</span>
-            <div className="flex text-skin-solid gap-3 items-center">
-                {values.map((value, i) => (
-                    <span
-                        className={`${
-                            isLabel
-                                ? 'text-right font-bold text-skin-solid'
-                                : 'text-skin-mild'
-                        } text-xs  font-bold`}
-                    >
-                        {isLabel ? value : `RS ${value.toFixed(2)}`}
-                    </span>
-                ))}
-            </div>
+        <div className={`${isLabel ? 'w-36': 'w-24'} flex flex-col text-skin-solid gap-3 items-center`}>
+            <span
+                className={`${
+                    title === '' ? 'h-5' : ''
+                } w-24 text-xs xl:text-sm 2xl:text-base font-bold text-right`}
+            >
+                {title}
+            </span>
+            {values.map((value, i) => (
+                <span
+                    className={`${
+                        isLabel
+                            ? 'font-bold text-skin-solid text-sm'
+                            : 'text-skin-mild text-xs text-right'
+                    }  h-5 font-bold border-b-[1px] w-full`}
+                >
+                    {isLabel
+                        ? value
+                        : `R$ ${formatString(value, value.length-2)}`}
+                </span>
+            ))}
         </div>
     );
 };
 
 const SpreadSheet: React.FC<{ title: string }> = ({ title }) => {
-    const data = [1800340, 168525, 156200];
+    const info = [1800340, 168525, 156200, 245810, 1478505, 2584101];
+    let data = info.map(item => item.toString())
+
     return (
-        <div className="bg-skin-white px-5 py-9">
+        <div className="flex flex-col bg-skin-white px-5 py-9 w-[332px] xl:w-[524px]">
             <span className="font-bold mb-4">{title}</span>
-            <Line title={''} values={[2019, 2020, 2021]} isLabel={true}/>
-            <Line title={'Patrimônio Líquido'} values={data} />
-            <Line title={'Faturamento'} values={data} />
-            <Line title={'EBITDA'} values={data} />
-            <Line title={'Lucro Líquido'} values={data} />
-            <Line title={'Endividamento CP'} values={data} />
-            <Line title={'Endividamento LP'} values={data} />
+            <div className="flex ">
+                <Column
+                    title={''}
+                    values={[
+                        'Patrimônio Líquido',
+                        'Faturamento',
+                        'EBITDA',
+                        'Lucro Líquido',
+                        'Endividamento CP',
+                        'Endividamento LP',
+                    ]}
+                    isLabel={true}
+                />
+                <div className="flex overflow-x-scroll scrollbar-hide">
+                <Column title={'2019'} values={data} />
+                <Column title={'2020'} values={data} />
+                <Column title={'2021'} values={data} isTail={true} />
+                </div>
+               
+            </div>
         </div>
     );
 };
