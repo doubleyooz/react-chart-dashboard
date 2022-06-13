fffd8501cddc7a95d0e4132202b12bd5a2d3b1ae
Waifu Forever <itachiut1r4@gmail.com>
2022-06-13 13:40:44 -0300

    fix: label container does not occupy full width on screens lower than 450px

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,8 +7,8 @@ const Header: React.FC<{}> = () => {
     return (
         <header className="mt-6 mb-24 md:mb-28 lg:mb-32 gap-1 px-6 flex flex-col xs:flex-row justify-between z-350 bg-transparent">
             <div className="flex flex-col justify-start gap-1 mb-2">
-                <span className="text-xs text-white">Pages / Profile</span>
-                <span className="text-sm text-white font-bold">Profile</span>
+                <span className="text-[10px] xs:text-xs text-white">Pages / Profile</span>
+                <span className="text-xs xs:text-sm text-white font-bold">Profile</span>
             </div>
             <div className="flex flex-col xs:flex-row justify-center items-center gap-3 2xs:gap-6">
                 <div className="flex items-center">
@@ -21,7 +21,7 @@ const Header: React.FC<{}> = () => {
                         />
                     </div>
                 </div>
-                <div className='flex items-center mr-2 gap-3 -translate-y-24 xs:translate-y-0 0xs:translate-x-12 xxs:translate-x-20 2xxs:translate-x-24 xs:translate-x-0'>
+                <div className='flex items-center mr-2 gap-2 -translate-y-24 xs:translate-y-0 0xs:translate-x-12 xxs:translate-x-20 2xxs:translate-x-24 xs:translate-x-0'>
                     <div className="flex items-center gap-[5px]">
                         <User className="fill-skin-default" />
                         <span className="text-xs text-white">Rafael</span>
